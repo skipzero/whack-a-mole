4248136aa12c0bdc6b8468504e60be742ce90f7f
Skip McBride <skip@zerosquadron.com>
2015-08-07 19:40:12 -0700

    timer and markup added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,52 @@
 /*global $, require, _, console */
 'use strict';
 
-var game = { 
 
-};
+function startTimer() {
+	var s 		= 0
+	, m 		= 0
+	, counter 	= window.setInterval(function() {
+		s++;
 
-window.Game = game.render();
+
+		if (s === 59) {
+			s = 0;
+			m++;
+		}
+
+		s = addZed(s);
+
+		if (m >= 3) {
+			stopTimer(m, counter);
+		}
+		document.getElementById('content').innerHTML = m + ':' + s;
+
+	}, 1000);
+}
+
+function addZed(s) {
+	if (s < 10){
+		s = '0' + s;
+		return s;
+	}
+	return s;
+}
+
+	//TODO: fix incrementing...
+function minutes(s) {
+	var m = (s/60).toString();
+	s = 0;
+	m = parseInt(m, 10);
+	return m;
+}
+
+function stopTimer(s, counter) {
+	window.clearInterval(counter);
+}
+
+function randomNumb(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+startTimer();
